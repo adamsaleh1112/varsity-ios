23ce54865ac1303461da5b84c95b1e369812f395
Adam <adamsalehdev@gmail.com>
2026-02-05 10:52:41 -0500

    Initial Commit

---
+++ Varsity/VarsityApp.swift
@@ -0,0 +1,17 @@
+//
+//  VarsityApp.swift
+//  Varsity
+//
+//  Created by Adam Saleh on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct VarsityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
